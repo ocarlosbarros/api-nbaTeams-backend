5a19ec2d7610df143eed012015adff585903e373
Carlos Barros <carlos_dbs@hotmail.com>
2022-05-05 15:43:42 -0300

    feat:create server with express

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+require('dotenv').config();
+
+const Router = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(Router);
+
+app.listen(PORT, () => console.log(`running on port: ${ PORT }`));
